5cb4020242138724a61924019c3f2f9509fc8eb0
Wataru823 <voyage0823@gmail.com>
2022-07-12 18:36:24 +0900

    feat: employee.htmlでのtableの見た目を整える

--- src/main/resources/static/user-table.vue
+++ src/main/resources/static/user-table.vue
@@ -1,33 +1,33 @@
 <template>
-  <div>
-    <div>
-      <label>User Name</label>
-      <input v-model="addUser_name" />
-      <label>Password</label>
-      <input v-model="addUser_password" />
-      <button @click="handleAddUser">Add User</button>
-    </div>
-    <div>
-      <table>
-        <thead>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Password</th>
-          <th></th>
-        </thead>
-        <tbody>
-          <tr v-for="user in users">
-            <td>{{ user.id }}</td>
-            <td>{{ user.name }}</td>
-            <td>{{ user.password }}</td>
-            <td>
-              <button width="100" height="100" @click="handleDelUser(user.id)">
-                Del
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="container">
+    <div class="card mb-5">
+      <div class="card-header h5">User Management</div>
+      <div class="card-body">
+        <label>User Name</label>
+        <input v-model="addUser_name" />
+        <label>Password</label>
+        <input v-model="addUser_password" />
+        <button type="button" class="btn btn-outline-primary" @click="handleAddUser">Add User</button>
+        <hr class="mb-5">
+
+        <table class="table table-striped table-hover">>
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Password</th>
+            <th></th>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.id }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.password }}</td>
+              <td><button type="button" class="btn btn-outline-danger" width="100" height="100"
+                  @click="handleDelUser(user.id)">Del</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
