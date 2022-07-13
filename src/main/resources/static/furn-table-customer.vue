41e72ccafb4b787989eb91ff35851f424bb49e4a
tong-poo <hrak1529@gmail.com>
2022-07-13 13:59:05 +0900

    Enabled customer to specify how much furniture to lease

--- src/main/resources/static/furn-table-customer.vue
+++ src/main/resources/static/furn-table-customer.vue
@@ -11,13 +11,16 @@
           <th></th>
         </thead>
         <tbody>
-          <tr v-for="furn in furns">
+          <tr v-for="furn in computedFurns">
             <td>{{ furn.id }}</td>
             <td>{{ furn.name }}</td>
             <td>{{ furn.amount }}</td>
             <td>{{ furn.amount - furn.leasedAmount }}</td>
             <td><img width="100" height="100" v-bind:src="furn.imgUrl" /></td>
-            <td><button @click="handleLease(furn.id)">Lease</button></td>
+            <td>
+              <input type="number" v-model="furn.inputAmount" />
+              <button @click="handleLease(furn.id, furn.inputAmount)">Lease</button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -28,15 +31,22 @@
 module.exports = {
   props: ["furns"],
   data() {
-    return {};
+    return {
+      amounts: []
+    };
   },
   methods: {
-    async handleLease(furnId) {
-      await fetch(`/api/lease?furniture_id=${furnId}&amount=${3}`, {
+    async handleLease(furnId, amount) {
+      await fetch(`/api/lease?furniture_id=${furnId}&amount=${amount}`, {
         method: "PUT",
       });
       this.$emit("furn-leased");
     },
   },
+  computed: {
+    computedFurns(){
+      return this.furns.map(furn => ({...furn, inputAmount: 0}))
+    }
+  }
 };
 </script>
