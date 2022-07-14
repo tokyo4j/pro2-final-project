21153258680cb80ec2a559569919895c2b34cf98
Wataru823 <voyage0823@gmail.com>
2022-07-14 23:16:02 +0900

    feat: headerにSing Outを追加

--- src/main/resources/static/header.vue
+++ src/main/resources/static/header.vue
@@ -9,12 +9,8 @@
 
                 <ul class="nav nav-pills">
                     <li class="nav-item">
-                        <a href="#" class="nav-link active" aria-current="page">Home</a>
+                        <a href="./" class="nav-link active" aria-current="page">Sing Out</a>
                     </li>
-                    <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
                 </ul>
             </header>
         </div>
