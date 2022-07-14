4c95f55de0bba5c94a5b819b51c23716ff17ec9f
Wataru823 <voyage0823@gmail.com>
2022-07-15 00:17:42 +0900

    refactor: headerにicon画像を追加

--- src/main/resources/static/header.vue
+++ src/main/resources/static/header.vue
@@ -2,14 +2,14 @@
     <div>
         <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <!-- <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg> -->
+                <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <img src="./icon.jpg" alt="" style="width: 50px; height: 40px;">
                     <span class="fs-4">Furniture Rending System</span>
                 </a>
 
                 <ul class="nav nav-pills">
                     <li class="nav-item">
-                        <a href="./" class="nav-link active" aria-current="page">Sing Out</a>
+                        <a href="./" class="nav-link active" aria-current="page">Logout</a>
                     </li>
                 </ul>
             </header>
