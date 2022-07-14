d8808ac996d48bdcb137496a3cda6d51e73d7c09
Wataru823 <voyage0823@gmail.com>
2022-07-15 00:49:50 +0900

    docs: headerのタイトルをRendingからRentalに変更

--- src/main/resources/static/header.vue
+++ src/main/resources/static/header.vue
@@ -4,7 +4,7 @@
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                     <img src="./icon.jpg" alt="" style="width: 50px; height: 40px;">
-                    <span class="fs-4">Furniture Rending System</span>
+                    <span class="fs-4">Furniture Rental System</span>
                 </a>
 
                 <ul class="nav nav-pills">
