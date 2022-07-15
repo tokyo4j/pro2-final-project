ec7dbb3b34997ebf878ceeda2e6bc6db6155eef4
tong-poo <hrak1529@gmail.com>
2022-07-16 00:40:56 +0900

    Refactored

--- src/main/resources/static/lease-table-customer.vue
+++ src/main/resources/static/lease-table-customer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <p class="fs-5 m-4 mt-5" style="font-family: Georgia, serif;">When you have completed the return, press the button.</p>
+    <p class="fs-5 m-4 mt-5" style="font-family: Georgia, serif;">When you have completed the return, press the button.
+    </p>
     <div class="card mb-5">
       <div class="card-header h5 mb-4">Lease List</div>
       <table class="table table-striped table-hover">
@@ -16,11 +17,12 @@
             <td>{{ lease.furn.id }}</td>
             <td>{{ lease.furn.name }}</td>
             <td>{{ lease.amount }}</td>
-            <td>
-              <img width="100" height="100" v-bind:src="lease.furn.imgUrl" />
+            <td class="text-center">
+              <img height="100" v-bind:src="lease.furn.imgUrl" />
             </td>
-            <td><button type="button" class="m-4 btn btn-success"
-                @click="handleReturn(lease.id)">Return</button></td>
+            <td>
+              <button type="button" class="m-4 btn btn-success" @click="handleReturn(lease.id)">Return</button>
+            </td>
           </tr>
         </tbody>
       </table>
