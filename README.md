eb7ea672b8a2f0585329e218f3e46465cb2f925c
tong-poo <hrak1529@gmail.com>
2022-07-28 20:51:29 +0900

    Updated document

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+This project is made as a final project of プログラミング第2同演習.
+
 To run the application:
 ```bash
 ./mvnw spring-boot:run
 ```
+Then access <http://localhost:8080> in your browser.
 
-You can log in as employee using username "admin" and password "admin".
+You can log in as an employee using username "admin" and password "admin".
--- src/main/java/jp/ac/keio/pro2finalproject/config/StaticResourceConfiguration.java
+++ src/main/java/jp/ac/keio/pro2finalproject/config/StaticResourceConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StaticResouceConfiguration implements WebMvcConfigurer {
+public class StaticResourceConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
